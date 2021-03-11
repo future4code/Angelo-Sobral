666c84827dba636d2d02d112428025fc65f46375
Angelo O'Dwyer <70985334+AngeloVSO@users.noreply.github.com>
2021-03-11 17:22:55 -0300

    Tirei o post do jsx e coloquei num estado usando map

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -3,24 +3,36 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    Post:[
+    {
+      nomeUsuario: 'paulinha',
+      fotoUsuario: 'https://picsum.photos/50/50',
+      fotoPost: 'https://picsum.photos/200/150'
+    },
+    {
+      nomeUsuario: 'angeloOdwyer',
+      fotoUsuario: 'https://avatars.githubusercontent.com/u/70985334?s=460&u=b0db5806864c8b5337772b0b63f8519229f3521d&v=4',
+      fotoPost: 'https://picsum.photos/200/300/?blur=2'
+    },
+    {
+      nomeUsuario: 'karinerOdwyer',
+      fotoUsuario: 'https://source.unsplash.com/user/erond',
+      fotoPost: 'https://source.unsplash.com/random'
+    }
+  ]
+  }
+
+
   render() {
+
+    const listaDePost = this.state.Post.map(post => {
+      return <Post />
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'angeloOdwyer'}
-          fotoUsuario={'https://avatars.githubusercontent.com/u/70985334?s=460&u=b0db5806864c8b5337772b0b63f8519229f3521d&v=4'}
-          fotoPost={'https://picsum.photos/200/300/?blur=2'}
-        />
-        <Post
-          nomeUsuario={'karinerOdwyer'}
-          fotoUsuario={'https://source.unsplash.com/user/erondu'}
-          fotoPost={'https://source.unsplash.com/random'}
-        />
+        {listaDePost}
       </div>
     );
   }
