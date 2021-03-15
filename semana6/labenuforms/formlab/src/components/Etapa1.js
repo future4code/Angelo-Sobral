22b1bb738217e729e48185aa3bd9f77c5727aff7
Angelo O'Dwyer <70985334+AngeloVSO@users.noreply.github.com>
2021-03-15 20:54:18 -0300

    inseri verificação de preenchimento na etapa1

--- semana6/labenuforms/formlab/src/components/Etapa1.js
+++ semana6/labenuforms/formlab/src/components/Etapa1.js
@@ -8,12 +8,18 @@ margin: 0 auto;
 const Botao = styled.button`
 margin: 40px 10px;
 `
+const ParagrafoErro = styled.p`
+color: red;
+`
 
 export default class Etapa1 extends React.Component {
     state ={
         inputNome:'',
         inputIdade:'',
-        inputEmail:''
+        inputEmail:'',
+        erroNome:'',
+        erroIdade:'',
+        erroEmail:''
     }
 
     valorInputNome = (e) => {
@@ -31,10 +37,16 @@ export default class Etapa1 extends React.Component {
     verificaResposta = () => {
         if (this.state.inputNome && this.state.inputIdade && this.state.inputEmail) {
             this.props.botaoProximo()
-        } else {alert('Preencha todos os campos antes de ir para a próxima etapa.')}
+        } else {
+            alert('Preencha todos os campos antes de ir para a próxima etapa.')
+        if (!this.state.inputNome) {this.setState({erroNome: 'Preencha o nome'})} else {this.setState({erroNome: ''})}
+        if (!this.state.inputIdade) {this.setState({erroIdade: 'Preencha a idade'})} else {this.setState({erroIdade: ''})}
+        if (!this.state.inputEmail) {this.setState({erroEmail: 'Preencha o e-mail'})} else {this.setState({erroEmail: ''})}
+        }
     }
 
     render() {
+        
         return (
             <div>
                 <Titulo>Etapa1: Dados Gerais</Titulo>
@@ -43,16 +55,20 @@ export default class Etapa1 extends React.Component {
                 onChange={this.valorInputNome}
                 value={this.state.inputNome}
                 />
+                {/* fiz assim pra testar outra forma */}
+                {!this.state.erroNome ? (<p></p>) : (<ParagrafoErro>{this.state.erroNome}</ParagrafoErro>)}
                 <p>2. Qual sua idade?</p>
                 <input
                 onChange={this.valorInputIdade}
                 value={this.state.inputIdade}
                 />
+                {this.state.erroIdade && (<ParagrafoErro>{this.state.erroIdade}</ParagrafoErro>)}
                 <p>3. Qual seu e-mail?</p>
                 <input
                 onChange={this.valorInputEmail}
                 value={this.state.inputEmail}
                 />
+                {this.state.erroEmail && (<ParagrafoErro>{this.state.erroEmail}</ParagrafoErro>)}
                 <p>4. Qual a sua escolaridade?</p>
                 <select>
                     <option>Ensino médio incompleto</option>
--- semana6/labenuforms/formlab/src/components/Etapa2.js
+++ semana6/labenuforms/formlab/src/components/Etapa2.js
@@ -10,14 +10,43 @@ margin: 40px 10px;
 `
 
 export default class Etapa2 extends React.Component {
+    state = {
+        curso:'',
+        institnuicao:'',
+        erroCurso:'',
+        erroInstituicao:''
+    }
+
+    inputCurso = (e) => {
+        this.setState({curso: e.target.value})
+    }
+
+    inputIntituicao = (e) => {
+        this.setState({institnuicao: e.target.value})
+    }
+
+    verificaResposta = () => {
+        if (this.state.curso && this.state.institnuicao) {
+            this.props.botaoProximo()
+        } else {
+            alert('Preencha todos os campos antes de ir para próxima etapa.')
+        }
+    }
+
     render() {
         return (
             <div>
                 <Titulo>ETAPA 2: Informações educacionais para quem está cursando (ou já terminou) o ensino superior</Titulo>
                 <p>5. Qual curso?</p>
-                <input/>
+                <input
+                onChange={this.inputCurso}
+                value={this.state.curso}
+                />
                 <p>6. Qual instituição de ensino?</p>
-                <input/>
+                <input
+                onChange={this.inputIntituicao}
+                value={this.state.inputIntituicao}
+                />
                 <br/>
                 <Botao onClick={this.props.botaoVoltar}>Voltar Etapa</Botao>
                 <Botao onClick={this.props.botaoProximo}>Próxima Etapa</Botao>
