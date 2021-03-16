361aa82af2e52cecc64d9e6f9b4673d97f663d70
Angelo O'Dwyer <70985334+AngeloVSO@users.noreply.github.com>
2021-03-15 22:29:56 -0300

    ajustes na etapa3

--- semana6/labenuforms/formlab/src/components/Etapa3.js
+++ semana6/labenuforms/formlab/src/components/Etapa3.js
@@ -8,14 +8,39 @@ margin: 0 auto;
 const Botao = styled.button`
 margin: 40px 10px;
 `
+const ParagrafoErro = styled.p`
+color: red;
+`
 
 export default class Etapa3 extends React.Component {
+    state = {
+        naoFezGraduacao:'',
+        erroGraduacao:''
+    }
+
+    inputNaoFezGraduacao = (e) => {
+        this.setState({naoFezGraduacao: e.target.value})
+    }
+
+    verificaResposta = () => {
+        if (this.state.naoFezGraduacao) {
+            this.props.botaoProximo()
+        } else {
+            alert('Preencha todos os campos antes de ir para próxima etapa.')
+            if (!this.state.naoFezGraduacao) {this.setState({erroGraduacao: 'Preencha o motivo'})} else {this.setState({erroGraduacao: ''})}
+        }
+    }
+
     render() {
         return (
             <div>
                 <Titulo>ETAPA 3: Informações sobre quem não se formou no ensino superior nem está cursando</Titulo>
                 <p>5. Porque você não iniciou um curso de graduação?</p>
-                <input/>
+                <input
+                onChange={this.inputNaoFezGraduacao}
+                value={this.state.naoFezGraduacao}
+                />
+                {this.state.erroGraduacao && (<ParagrafoErro>{this.state.erroGraduacao}</ParagrafoErro>)}
                 <p>6. Você fez algum curso complementar?</p>
                 <select>
                     <option>Curso Técnico</option>
@@ -24,7 +49,7 @@ export default class Etapa3 extends React.Component {
                 </select>
                 <br/>
                 <Botao onClick={this.props.botaoVoltar}>Voltar Etapa</Botao>
-                <Botao onClick={this.props.botaoProximo}>Próxima Etapa</Botao>
+                <Botao onClick={this.verificaResposta}>Próxima Etapa</Botao>
             </div>
         )
     }
