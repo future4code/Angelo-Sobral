a7e89e821d84aa001fde261bc7f7017319ad6b95
Angelo O'Dwyer <70985334+AngeloVSO@users.noreply.github.com>
2021-04-05 16:03:05 -0300

    Coloqei bg color com gradient

---
+++ semana8/astromatch/astromatch/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        Card Astromatch
+      </div>
+    </div>
+  );
+}
+
+export default App;
