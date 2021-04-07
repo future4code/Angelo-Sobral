2d99247603b691a849d1280329a21718d91fef26
Angelo O'Dwyer <70985334+AngeloVSO@users.noreply.github.com>
2021-04-07 20:07:29 -0300

    ajustes no css botão reset

--- semana8/astromatch/astromatch/src/components/Buttons.js
+++ semana8/astromatch/astromatch/src/components/Buttons.js
@@ -1,11 +1,23 @@
+import axios from 'axios';
 import React from 'react'
 import "../App.css";
 
 const Buttons = (props) => {
 
+    const clearAll = async () => {
+        try {
+            const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/clear'
+            await axios.put(url)
+            alert("Tudo ok")
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return(
         <div className="buttons">
             <button onClick={() => props.choosePerson(props.id, false)} className="dislike"/>
+            <button onClick={clearAll} className="reset"/>
             <button onClick={() => props.choosePerson(props.id, true)} className="like"/>
         </div>
     )
