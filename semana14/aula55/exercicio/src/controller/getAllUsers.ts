b0caefa2dea984532a7e7c2c6e1f2afb8bf1e6b1
AngeloVSO <angelo_odwyer@hotmail.com>
2021-06-14 19:41:36 -0300

    Exercicios 2 e 3 feito

---
+++ semana14/aula55/exercicio/src/controller/getAllUsers.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { getUsers } from "../data/getUsers";
+import { getTokenData } from "../services/authenticator";
+
+export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const token = req.headers.authorization
+
+    const tokenData = getTokenData(token as string)
+
+    if(!tokenData){
+        throw new Error("Não autorizado.");
+     }
+
+    const allUsers = await getUsers()
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
--- semana14/aula55/exercicio/src/index.ts
+++ semana14/aula55/exercicio/src/index.ts
@@ -1,6 +1,9 @@
 import { app } from "./controller/app"
+import { getAllUsers } from "./controller/getAllUsers"
 import { login } from "./controller/login"
 import { signup } from "./controller/signup"
 
 app.post("/signup", signup)
 app.post("/login", login)
+
+app.get("/all", getAllUsers)
